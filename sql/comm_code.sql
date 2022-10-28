b53a1a8f3fd43d002dd4aa323a74bc325e2f77d1
SujinHuh <juin483@naver.com>
2022-10-28 16:11:26 +0900

    modify common_code sql

---
+++ sql/comm_code.sql
@@ -0,0 +1,9 @@
+CREATE TABLE comm_code
+(
+    group    VARCHAR(20),
+    code_name VARCHAR(20),
+    code     VARCHAR(20),
+    comment  VARCHAR(20),
+) ENGINE=InnoDB CHARSET=utf8;
+
+drop table comm_code;
