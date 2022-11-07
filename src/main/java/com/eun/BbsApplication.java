fbd926bb9dba0afc395bfff02b9750158bf52a4b
sujinhuh <sujin941220@gmail.com>
2022-11-07 21:23:59 +0900

    add freeboard html

--- src/main/java/com/eun/BbsApplication.java
+++ src/main/java/com/eun/BbsApplication.java
@@ -1,14 +1,13 @@
 package com.eun;
 
-import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EunApplication {
+public class BbsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EunApplication.class, args);
+        SpringApplication.run(BbsApplication.class, args);
     }
 
 }
