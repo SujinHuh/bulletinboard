5a68ef336252a9d99cdfd46115b82ae98cb4d4d4
SujinHuh <juin483@naver.com>
2022-12-27 13:58:08 +0900

    add freeboard/Modify (기능구현 중)

--- src/main/resources/static/js/board/view.js
+++ src/main/resources/static/js/board/view.js
@@ -5,39 +5,32 @@ import Modal from '/js/common/modal.js';
 
 class View {
 
-    constructor(prop) {
-        this.prop = prop;
+    async constructor() {
         this.dom = {
-            modifyBt : document.getElementById('modifyBt')
+            modifyBt: document.getElementById('modifyBt')
         };
-        this.init();
-
         console.log(location.pathname); // 현재 페이지의 경로를 반환
         let array = location.pathname.split('/');
 
         let seq = array[array.length - 1]; // BoardAction-view() 1. 해당 페이지로 들어와서 url에있는 seq를 parsing을 한다음 getView 함수에 전달
 
-        let contentNull = { //modal
-            title : '게시판',
-            body : '게시글 없음',
+        // 1.  modal을 쓰기에 가장먼저 로딩이되야된다.
+        this.setModal();
+        // 2. 게시글을 가져와야 이벤트를 바인딩할수있다.
+        await this.getView(seq);
+        // 3. 이벤트를 바인딩 할 태그 + 모달을 띄어야되니까 가장마지막에 이벤트를 바인딩한다.
+        if (this.myBbs) {
+            this.eventBind();
         }
-        this.notiModal = new Modal(contentNull);
-        this.getView(seq);
-        // this.setModal(); //클릭을 하면 수정,삭제 버튼이 나와야 하니까.
     }
-    init(){
-        this.eventBind();
+    eventBind() {   // 버튼에 이벤트는 모달을 셋팅하는게 아닌 선언된 모달의 함수를 이용해야된다.
+                    // 지금같은 경우는 open함수를 사용.
+        this.dom.modifyBt.addEventListener('click', () => this.modifyModal.open());
+
     }
-    eventBind() {
-        console.log('수정버튼 클릭');
-        this.prop.el.id.modifyBt.addEventListener('click',() => this.setModal());
-    }
-    setModal(){
-        let contentNull = { //modal
-            title : '게시판',
-            body : '게시글 없음',
-        }
-        let modifyModal = { // modifyModal
+
+    setModal(){  // 한번만 콜되야되는 함수
+        let modifyModal = {
             title: '수정',
             body : '수정하시겠습니까?',
             callback : [
@@ -50,17 +43,18 @@ class View {
                 }
             ]
         }
-        this.notiModal = new Modal(contentNull);
-        this.notiModal = new Modal(modifyModal);
+
+        this.modifyModal = new Modal(modifyModal);
 
         let notiParam = {
-            title : 'content View',
+            title : '게시판',
             body : '선택해주세요',
         }
         this.notiModal = new Modal(notiParam);
     }
 
-    getView(seq) {
+    //비동기 -> 동기
+    async getView(seq) {  // 마찬가지로 게시글을 가져오는 함수이기에 한번만 콜되야된다.
         let a = document.getElementById('contentArea'); //해당 tag에 접근
         //  BoardAction-view() 2. axios를 통해  method post로 아래 URL로 게시글번호를 전달한 상황입니다.
         axios.post('/board/view/' + seq)
@@ -70,15 +64,12 @@ class View {
                 console.log(res.data);  // java에서 내려준 responseVo
                 // res.data 가 저렇게 json으로 찍힘 data가 null이면
                 console.log(res.data.data); // responseVo에 set한 data
-                if(view.myBbs == true){
-                    let btn = '<button>수정</button>';
-                    a.addEventListener("click", (e) => this.modifyModal.open(e));
-                }
-                    let div = '<tr>' +
-                        `<td>${view.title}</td>` +
-                        `<td>${view.content}</td>` +
-                        '</tr>';
-                    a.insertAdjacentHTML("beforeend", div); //js로 dom 요소를 삽입
+                this.myBbs = view.myBbs;
+                let div = '<tr>' +
+                    `<td>${view.title}</td>` +
+                    `<td>${view.content}</td>` +
+                    '</tr>';
+                a.insertAdjacentHTML("beforeend", div); //js로 dom 요소를 삽입
             })
             .catch((res) => {
                 console.log(res);
@@ -90,11 +81,11 @@ class View {
 
     }
 
-    modify(type,target) {
+    modify(type,target) {   // 수정/삭제는 한번만 일어나겠죠? 그러니까 이것도 한번만 콜되야되는 함수.
         let seq = target.dataset.seq;
-        axios.post(`/todo/modify/${type}/${seq}`)
+        axios.post(`/board/modify/${seq}`)
             .then((res) => {
-                if(res.data.code === '0000') {
+                if(res.data.code === '0111') {
                     this.modifyModal.setBody('수정 완료');
                 } else {
                     this.modifyModal.setBody('수정 실패');
